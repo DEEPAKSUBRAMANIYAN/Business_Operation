debc896828848dcc4cdae5c8d2adc168d1253444
kevinJoshva <kevinjesus2162004@gmail.com>
2026-09-05 11:41:56 +0530

    role and companyuser

---
+++ bizflow/bizflow/src/main/java/com/business/bizflow/entity/CompanyUser.java
@@ -0,0 +1,8 @@
+package com.business.bizflow.entity;
+import com.business.bizflow.enums.Role;
+import jakarta.persistence.*;
+@Entity 
+@Table (name="company_user")
+public class CompanyUser {
+    
+}
